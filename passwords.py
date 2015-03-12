9cfb1fcb702f3b20267b27a7b24daf536ddda04c
Thomas Fenzl <thomas.fenzl@tngtech.com>
2015-03-12 18:56:54 +0100

    trampoline webpage, fixed environment with passwords

--- passwords.py
+++ passwords.py
@@ -2,7 +2,7 @@
 import uuid
 
 for i in range(1,8):
-    print ("PASSWORD_LEVEL{}={}".format(i, uuid.uuid4()))
-print("PASSWORD_FINAL=123443210987")
+    print ("export PASSWORD_LEVEL{}={}".format(i, uuid.uuid4()))
+print("export PASSWORD_FINAL=123443210987")
 
 
