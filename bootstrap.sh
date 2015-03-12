019debbdecc4d337b3607120603faa2363e65f41
Thomas Fenzl <thomas.fenzl@tngtech.com>
2015-03-12 16:23:42 +0100

    automated startup

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,15 @@ apt-get update
 apt-get install -y python-pip lxc-docker bind9 libconfig-auto-perl libunix-syslog-perl libjson-xs-perl libnet-dns-perl
 pip install docker-compose
 usermod -a -G docker vagrant
+cat > /etc/resolv.conf <<EOF
+nameserver 127.0.0.1
+search ctf.tng.retreat
+EOF
 cp /vagrant/dns/dns4docker.pl /usr/bin/
 cp -r /vagrant/dns/* /etc/bind/
 cp /vagrant/dns/dns4docker-upstart.conf /etc/init/
+cp /vagrant/ctf-levels.conf /etc/init
+cp -r /vagrant/levels /opt
+start dns4docker-upstart
+start ctf-levels
 service bind9 restart
